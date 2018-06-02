5ff2ee806f779c6844ab0a2381810757e6ac3659
John Green <jpgreen@Johns-iMac.local>
2018-06-01 22:11:20 -0400

    Lesson 2

--- window-shopper/Controller/MainVC.swift
+++ window-shopper/Controller/MainVC.swift
@@ -9,17 +9,29 @@
 import UIKit
 
 class MainVC: UIViewController {
-
+    
+    @IBOutlet weak var wageTxt: CurrencyTxtField!
+    @IBOutlet weak var priceTxt: CurrencyTxtField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcBtn.backgroundColor = #colorLiteral(red: 1, green: 0.6746097803, blue: 0.339186728, alpha: 1)
+        calcBtn.setTitle("calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func calculate() {
+        
+        print("We got here")
+        
     }
-
 
 }
 
--- window-shopper/Views/CurrencyTxtField.swift
+++ window-shopper/Views/CurrencyTxtField.swift
@@ -8,11 +8,24 @@
 
 import UIKit
 
+// set up Interface Builder to display code-defined views
+
+@IBDesignable
+
 class CurrencyTxtField: UITextField {
+    
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
 
     override func awakeFromNib() {
-        
         super.awakeFromNib()
+        customizeView()
+        }
+    
+    
+    func customizeView() {
+        
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2481271404)
         layer.cornerRadius = 5.0
         textAlignment = .center
@@ -22,9 +35,9 @@ class CurrencyTxtField: UITextField {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
             attributedPlaceholder = place
             textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-            
+        
         }
-              
-    }
 
+    }
+    
 }
